122303e5a7531f20741a1644bd5cba1fd284713d
Alessandro Fan <alefan@deloitte.it>
2020-06-21 22:01:09 +0200

    adapt homescreen with the template

--- screens/Home.js
+++ screens/Home.js
@@ -1,35 +1,58 @@
-import React from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 import {Text, View, StyleSheet, Dimensions, SafeAreaView} from 'react-native';
 import {colors} from '../configs';
 import {TouchableText, Button} from '../components';
+import {MainTemplate} from '../template';
 
-export function Home() {
+export function Home({navigation}) {
+  const [error, setError] = useState(false);
+
+  const color = error ? colors.salmon : colors.white;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: color,
+      },
+    });
+  });
+
   return (
-    <SafeAreaView style={[styles.container]}>
-      <View style={[styles.background]}>
+    <MainTemplate
+      backgroundColor={color}
+      buttonText={'CHECK'}
+      buttonOnPress={() => {
+        //TODO: VALIDATE THE URL
+        alert('ciao');
+      }}>
+      <View style={{flex: 1}}>
         <Text style={[styles.text]}>github.com</Text>
-        <TouchableText text={'user'} onPress={() => {}} />
-        <TouchableText text={'repo'} onPress={() => {}} />
+        <TouchableText
+          text={'user'}
+          onPress={() => {
+            navigation.navigate('User');
+          }}
+        />
+        <TouchableText
+          text={'repo'}
+          onPress={() => {
+            navigation.navigate('Repository');
+          }}
+        />
       </View>
-      <View>
-        <Button text={'CHECK'} />
-      </View>
-    </SafeAreaView>
+    </MainTemplate>
   );
 }
+
+Home.navigationOptions = {
+  headerStyle: {
+    backgroundColor: colors.salmon,
+  },
+};
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 35,
     fontWeight: '600',
-  },
-  container: {
-    paddingHorizontal: 15,
-  },
-  background: {
-    backgroundColor: colors.white,
-    paddingTop: 20,
-    paddingHorizontal: 15,
-    height: Dimensions.get('screen').height,
   },
 });
